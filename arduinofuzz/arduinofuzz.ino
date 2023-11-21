e44f380bd8f7f6c5fe0e62e69b8edd72eb52b2d9
aWZHY0yQH81uOYvH <32046476+aWZHY0yQH81uOYvH@users.noreply.github.com>
2023-11-21 14:57:00 -0800

    prepare Arduino for host control

--- arduinofuzz/arduinofuzz.ino
+++ arduinofuzz/arduinofuzz.ino
@@ -17,67 +17,62 @@ struct button_path_t {
 	int delay;
 };
 
-const button_path_t b_percent = {K5, K10, 500};
-const button_path_t b_sqrt    = {K8, K9, 500};
-const button_path_t b_off     = {K3, K1, 50};
-const button_path_t b_on      = {K3, K2, 130};
-const button_path_t b_mrc     = {K7, K10, 500};
-const button_path_t b_msub    = {K5, K0, 500};
-const button_path_t b_madd    = {K4, K0, 500};
-const button_path_t b_div     = {K4, K1, 500};
-const button_path_t b_7       = {K6, K10, 100};
-const button_path_t b_8       = {K6, K0, 100};
-const button_path_t b_9       = {K6, K1, 100};
-const button_path_t b_mul     = {K4, K2, 500};
-const button_path_t b_4       = {K7, K0, 100};
-const button_path_t b_5       = {K7, K1, 100};
-const button_path_t b_6       = {K7, K2, 100};
-const button_path_t b_sub     = {K6, K2, 500};
-const button_path_t b_1       = {K8, K0, 100};
-const button_path_t b_2       = {K8, K1, 100};
-const button_path_t b_3       = {K8, K2, 100};
-const button_path_t b_add     = {K5, K2, 500};
-const button_path_t b_0       = {K8, K10, 100};
-const button_path_t b_dot     = {K7, K9, 100};
-const button_path_t b_eq      = {K4, K10, 500};
+enum BUTTON_ID {
+	B_0,
+	B_1,
+	B_2,
+	B_3,
+	B_4,
+	B_5,
+	B_6,
+	B_7,
+	B_8,
+	B_9,
+	B_DOT,
+	B_EQ,
+	B_SUB,
+	B_ADD,
+	B_MUL,
+	B_DIV,
+	B_PERCENT,
+	B_SQRT,
+	B_MRC,
+	B_MSUB,
+	B_MADD,
+	B_OFF,
+	B_ON,
+	B_MAX
+};
 
-// Helper function to map button names to button paths
-button_path_t parse_button(String buttonName) {
-	buttonName.toLowerCase();  // Convert the button name to lowercase for case-insensitive comparison
-	Serial.print("pressing ");
-	Serial.println(buttonName);
+const button_path_t buttons[B_MAX] = {
+	{K8, K10, 100}, // B_0
+	{K8, K0,  100}, // B_1
+	{K8, K1,  100}, // B_2
+	{K8, K2,  100}, // B_3
+	{K7, K0,  100}, // B_4
+	{K7, K1,  100}, // B_5
+	{K7, K2,  100}, // B_6
+	{K6, K10, 100}, // B_7
+	{K6, K0,  100}, // B_8
+	{K6, K1,  100}, // B_9
+	{K7, K9,  100}, // B_DOT
+	{K4, K10, 500}, // B_EQ
+	{K6, K2,  500}, // B_SUB
+	{K5, K2,  500}, // B_ADD
+	{K4, K2,  500}, // B_MUL
+	{K4, K1,  500}, // B_DIV
+	{K5, K10, 500}, // B_PERCENT
+	{K8, K9,  500}, // B_SQRT
+	{K7, K10, 500}, // B_MRC
+	{K5, K0,  500}, // B_MSUB
+	{K4, K0,  500}, // B_MADD
+	{K3, K1,  50},  // B_OFF
+	{K3, K2,  130}  // B_ON
+};
 
-	// Use a switch-case statement to map button names to button paths
-	if (buttonName == "percent") return b_percent;
-	else if (buttonName == "sqrt") return b_sqrt;
-	else if (buttonName == "off") return b_off;
-	else if (buttonName == "on") return b_on;
-	else if (buttonName == "mrc") return b_mrc;
-	else if (buttonName == "msub") return b_msub;
-	else if (buttonName == "madd") return b_madd;
-	else if (buttonName == "div") return b_div;
-	else if (buttonName == "7") return b_7;
-	else if (buttonName == "8") return b_8;
-	else if (buttonName == "9") return b_9;
-	else if (buttonName == "mul") return b_mul;
-	else if (buttonName == "4") return b_4;
-	else if (buttonName == "5") return b_5;
-	else if (buttonName == "6") return b_6;
-	else if (buttonName == "sub") return b_sub;
-	else if (buttonName == "1") return b_1;
-	else if (buttonName == "2") return b_2;
-	else if (buttonName == "3") return b_3;
-	else if (buttonName == "add") return b_add;
-	else if (buttonName == "0") return b_0;
-	else if (buttonName == "dot") return b_dot;
-	else if (buttonName == "eq") return b_eq;
+void press_button(BUTTON_ID button) {
+	const button_path_t &b = buttons[button];
 
-	// Default case: return an invalid button path
-	return {-1, -1};
-}
-
-
-void press_button(button_path_t b) {
 	const int voltageThreshold = 200;
 	unsigned long currentTime = millis();
 
@@ -105,34 +100,24 @@ void setup() {
 	Serial.begin(9600);  // Set the baud rate to match your serial monitor
 	for (int i = 0; i < 8; i++)
 		pinMode(i, INPUT);
-	press_button(b_on);
+	press_button(B_ON);
 }
 
 void loop() {
-	if (Serial.available() > 0) {
-		String userInput = Serial.readStringUntil('\n');
+	if (Serial.available()) {
+		const int selection = Serial.parseInt();
 
-		button_path_t buttonPath = parse_button(userInput);
+		if (selection < 0 || selection >= B_MAX) {
+			Serial.print("N");
+			return;
+		}
 
-		press_button(buttonPath);
+		press_button(selection);
+		Serial.print("K");
+
+		// Remove other characters
+		while (Serial.available()
+		       && (Serial.peek() < '0' || Serial.peek() > '9'))
+			Serial.print(Serial.read());
 	}
-
-	// http://blog.presentandcorrect.com/250-words-you-can-spell-with-a-calculator
-	press_button(b_on);
-	press_button(b_1);
-	delay(1000);
-	press_button(b_on);
-	press_button(b_7);
-	press_button(b_7);
-	press_button(b_3);
-	press_button(b_5);
-	delay(1000);
-	press_button(b_on);
-	press_button(b_5);
-	press_button(b_7);
-	press_button(b_7);
-	press_button(b_3);
-	press_button(b_4);
-	press_button(b_5);
-	delay(1000);
 }
