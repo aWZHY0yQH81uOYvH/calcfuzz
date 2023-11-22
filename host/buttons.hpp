77886e62b87b25c5b2b9ab09d5bdc4f8ae74c718
aWZHY0yQH81uOYvH <32046476+aWZHY0yQH81uOYvH@users.noreply.github.com>
2023-11-21 16:13:56 -0800

    separate button defs

---
+++ host/buttons.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+// Button IDs the Arduino sketch understands
+enum BUTTON_ID {
+	B_0,
+	B_1,
+	B_2,
+	B_3,
+	B_4,
+	B_5,
+	B_6,
+	B_7,
+	B_8,
+	B_9,
+	B_DOT,
+	B_EQ,
+	B_SUB,
+	B_ADD,
+	B_MUL,
+	B_DIV,
+	B_PERCENT,
+	B_SQRT,
+	B_MRC,
+	B_MSUB,
+	B_MADD,
+	B_OFF,
+	B_ON,
+	B_MAX
+};
--- host/calccomm.hpp
+++ host/calccomm.hpp
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include "buttons.hpp"
+
 #include <termios.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -53,34 +55,6 @@ public:
 		if(portfd >= 0)
 			close(portfd);
 	}
-
-	// Button IDs the Arduino sketch understands
-	enum BUTTON_ID {
-		B_0,
-		B_1,
-		B_2,
-		B_3,
-		B_4,
-		B_5,
-		B_6,
-		B_7,
-		B_8,
-		B_9,
-		B_DOT,
-		B_EQ,
-		B_SUB,
-		B_ADD,
-		B_MUL,
-		B_DIV,
-		B_PERCENT,
-		B_SQRT,
-		B_MRC,
-		B_MSUB,
-		B_MADD,
-		B_OFF,
-		B_ON,
-		B_MAX
-	};
 
 	// Press a button and wait for release
 	// Return false on failure
